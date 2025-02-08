f6d69480cb3d5271dc056d57dc7f62e4cff61ff2
PrajwalArali <prajwalarali12@gmail.com>
2025-02-08 12:03:17 +0530

    deployment changes2

--- render-build.sh
+++ render-build.sh
@@ -5,10 +5,11 @@ echo "Updating package lists..."
 apt-get update
 
 echo "Installing system dependencies..."
-apt-get install -y libgirepository1.0-dev gir1.2-gtk-3.0 brltty
+apt-get install -y libgirepository1.0-dev gir1.2-gtk-3.0 brltty liblouis-bin
 
 echo "Removing unavailable Python dependencies..."
 sed -i '/^Brlapi==/d' requirements.txt  # Remove Brlapi if present
+sed -i '/^louis==/d' requirements.txt  # Remove louis if present
 
 echo "Installing Python dependencies..."
 pip install -r requirements.txt
