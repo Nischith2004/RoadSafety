f7ccc989d2f3e0de6e117cccffcceaffbdf03f26
PrajwalArali <prajwalarali12@gmail.com>
2025-02-08 12:08:50 +0530

    deployment changes4

--- render-build.sh
+++ render-build.sh
@@ -5,12 +5,13 @@ echo "Updating package lists..."
 apt-get update
 
 echo "Installing system dependencies..."
-apt-get install -y libgirepository1.0-dev gir1.2-gtk-3.0 brltty liblouis-bin libsnmp-dev snmp 
+apt-get install -y libgirepository1.0-dev gir1.2-gtk-3.0 brltty liblouis-bin libsnmp-dev snmp libpwquality-dev 
 
 echo "Removing unavailable Python dependencies..."
-sed -i '/^Brlapi==/d' requirements.txt   # Remove Brlapi if present
-sed -i '/^louis==/d' requirements.txt    # Remove louis if present
-sed -i '/^netsnmp-python==/d' requirements.txt  # Remove netsnmp-python if present
+sed -i '/^Brlapi==/d' requirements.txt        # Remove Brlapi if present
+sed -i '/^louis==/d' requirements.txt         # Remove louis if present
+sed -i '/^netsnmp-python==/d' requirements.txt # Remove netsnmp-python if present
+sed -i '/^pwquality==/d' requirements.txt     # Remove pwquality if present
 
 echo "Installing Python dependencies..."
 pip install -r requirements.txt
