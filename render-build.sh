c12bded4d74de1a559ee8bf6082b00b894b6f045
PrajwalArali <prajwalarali12@gmail.com>
2025-02-08 12:18:19 +0530

    deployment changes6

--- render-build.sh
+++ render-build.sh
@@ -6,9 +6,9 @@ apt-get update
 
 echo "Installing system dependencies..."
 apt-get install -y \
+    gobject-introspection \
     libgirepository1.0-dev \
     gir1.2-gtk-3.0 \
-    gobject-introspection \
     libffi-dev \
     libcairo2-dev \
     pkg-config \
@@ -16,7 +16,13 @@ apt-get install -y \
     libpango1.0-dev \
     libatk1.0-dev \
     libgdk-pixbuf2.0-dev \
-    libglib2.0-dev
+    libglib2.0-dev \
+    build-essential \
+    meson \
+    ninja-build
+
+echo "Checking if gobject-introspection is installed..."
+pkg-config --exists gobject-introspection-1.0 && echo "gobject-introspection found" || echo "gobject-introspection NOT found"
 
 echo "Removing unavailable Python dependencies..."
 sed -i '/^Brlapi==/d' requirements.txt        # Remove Brlapi if present
@@ -25,4 +31,5 @@ sed -i '/^netsnmp-python==/d' requirements.txt # Remove netsnmp-python if presen
 sed -i '/^pwquality==/d' requirements.txt     # Remove pwquality if present
 
 echo "Installing Python dependencies..."
+pip install --upgrade pip
 pip install -r requirements.txt
