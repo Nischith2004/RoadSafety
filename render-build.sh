91d6082ce75e13087970e10868da38c7aeb106d8
PrajwalArali <prajwalarali12@gmail.com>
2025-02-08 11:57:49 +0530

    deployment changes

--- render-build.sh
+++ render-build.sh
@@ -5,7 +5,10 @@ echo "Updating package lists..."
 apt-get update
 
 echo "Installing system dependencies..."
-apt-get install -y libgirepository1.0-dev gir1.2-gtk-3.0
+apt-get install -y libgirepository1.0-dev gir1.2-gtk-3.0 brltty
+
+echo "Removing unavailable Python dependencies..."
+sed -i '/^Brlapi==/d' requirements.txt  # Remove Brlapi if present
 
 echo "Installing Python dependencies..."
 pip install -r requirements.txt
